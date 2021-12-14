95a2ebddcbf93d2d41b41d252f23ade2e682b6ff
libra10042 <libra10042@gmail.com>
2021-12-15 01:10:19 +0900

    폴더 생성

---
+++ baekjon_Algorism/src/programmers/level1/k번쨰수.java
@@ -0,0 +1,12 @@
+package programmers.level1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class k번쨰수 {
+    public static void main(String[] args) {
+        BufferedReader s = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+
+}
