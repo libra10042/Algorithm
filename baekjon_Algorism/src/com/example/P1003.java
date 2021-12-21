6a4027fad8b69d8f0322c339187a717d68a527aa
libra10042 <libra10042@gmail.com>
2021-12-22 00:40:59 +0900

    P1003:피보나치(DP)

---
+++ baekjon_Algorism/src/com/example/P1003.java
@@ -0,0 +1,46 @@
+package com.example;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+public class P1003 {
+
+    static Integer[][] dp = new Integer[41][2];
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        dp[0][0] = 1; // N=0 일 때의 0 호출 횟수.
+        dp[0][1] = 0; // N=0 일 때의 1 호출 횟수.
+        dp[1][0] = 0; // N=1 일 떄의 0 호출 횟수.
+        dp[1][1] = 1; // N=1 일 떄의 1 호출 횟수.
+
+        int a = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        while(a-- >0){
+            int N = Integer.parseInt(br.readLine());
+            fibonacci(N);
+            sb.append(dp[N][0] + " " + dp[N][1]).append('\n');
+        }
+        System.out.println(sb);
+
+    }
+
+
+    static Integer[] fibonacci(int N){
+        // N에 대한 0, 1의 호출 횟수가 없을 때 (탐색하지 않은 값일 때)
+        if(dp[N][0] == null || dp[N][1] == null){
+            // 각 N에 대한 0 호출 횟수와 1 호출 횟수를 재귀호출한다.
+            dp[N][0] = fibonacci(N-1)[0] + fibonacci(N-2)[0];
+            dp[N][1] = fibonacci(N-1)[1] + fibonacci(N-2)[1];
+        }
+        // N에 대한 0과 1, 즉 [N][0] 과 [N][1] 을 담고 있는 [N]을 반환한다.
+        return dp[N];
+    }
+}
